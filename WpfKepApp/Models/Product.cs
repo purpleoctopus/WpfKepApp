d267dbea72f2cd73ad73c9dd6ebb3f802115cdec
denys <denys@MEGAPAVUK>
2024-11-13 21:58:00 +0200

    Add project files.

---
+++ WpfKepApp/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WpfKepApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+
+        public Product(string name, decimal price, decimal discount)
+        {
+            Name = name;
+            Price = price;
+            Discount = discount;
+        }
+
+        public Product() : this("Новий товар", 0, 0) { }
+
+        public string GetFormattedDiscount{ 
+            get => Discount == 0 ? "Немає" : Discount.ToString() + "%";
+        }
+        public string GetPriceAfterDiscount
+        {
+            get => Price * (100 - Discount) /100 + "";
+        }
+        public override string ToString()
+        {
+            return $"{Name} - Ціна без знижки: {Price}, Знижка: {GetFormattedDiscount}, " +
+                $"Остаточна ціна: {GetPriceAfterDiscount}";
+        }
+    }
+}
---
+++ WpfKepApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using WpfKepApp.Models;
+
+namespace WpfKepApp
+{
+    public partial class MainWindow : Window
+    {
+        private List<Product> products;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadProducts();
+            productDataGrid.ItemsSource = products;
+        }
+
+        private void LoadProducts()
+        {
+            products = new List<Product>
+            {
+                new Product("Телефон", 12000, 0),
+                new Product("Ноутбук", 25000, 15),
+                new Product("Навушники", 1000, 10),
+                new Product("Монітор", 6000, 0)
+            };
+        }
+
+        public void FindMinPriceProduct_Click(object sender, RoutedEventArgs e)
+        {
+            if (products.Any())
+            {
+                // Знаходимо товар з мінімальною ціною
+                var minPriceProduct = products.OrderBy(p => p.GetPriceAfterDiscount).First();
+                minPriceProductText.Text = $"Товар з мінімальною ціною: {minPriceProduct}";
+            }
+            else
+            {
+                minPriceProductText.Text = "Список товарів порожній.";
+            }
+        }
+    }
+}
